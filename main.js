4d024a495cbd89d0b3e0a3fb016726e77232c3c4
sultanmyrza <sultanmyrza@sultanmrzasiMac.Home>
2018-12-26 15:41:48 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+if (window.location.toString().includes("oursky.com")) {
+    let technologies = $(".tech");
+    let newTechnology = `<a href="https://flutter.io/" target="_blank"><img src="https://flutter.io/assets/flutter-lockup-4cb0ee072ab312e59784d9fbf4fb7ad42688a7fdaea1270ccf6bbf4f34b7e03f.svg"></a>`;
+    technologies.append(newTechnology);
+}
