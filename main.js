d7da9b0fc5ebbf9455d797625bf06492d095b041
sultanmyrza <sultanmyrza@sultanmrzasiMac.Home>
2018-12-31 17:38:18 +0800

    add script to remove ads from intervaltimer.com

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ if (window.location.toString().includes("oursky.com")) {
     let newTechnology = `<a href="https://flutter.io/" target="_blank"><img src="https://flutter.io/assets/flutter-lockup-4cb0ee072ab312e59784d9fbf4fb7ad42688a7fdaea1270ccf6bbf4f34b7e03f.svg"></a>`;
     technologies.append(newTechnology);
 }
+
+if (window.location.toString().includes('http://www.intervaltimer.com/timers/')) {
+    $('.navbar').remove();
+    $('.feature').remove();
+    $('.navbar').remove();
+    $('.navbar').remove();
+    $('.footer').remove();
+    $('.col-md-4').remove();
+}
